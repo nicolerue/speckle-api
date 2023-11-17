b3b2fa5d2df06065e13f43f5c9df4f2a4d4b8112
Nicole Rue <nicolerue92@gmail.com>
2023-11-17 09:45:05 -0800

    revert

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ MongoClient.connect(connectionString)
     const db = client.db("speckle");
     const collectionTeachers = db.collection("speckle-teachers");
 
-    app.get("/api", (req, res) => {
+    app.get("/home", (req, res) => {
       res.status(200).json("HI! Speckle Api!");
     });
 
